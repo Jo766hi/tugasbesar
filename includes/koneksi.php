2441b8b4c7eb6aee996fabade9e9ae172662037c
HelloitsMutiaa <89656134+HelloitsMutiaa@users.noreply.github.com>
2021-11-29 11:04:04 +0700

    update variabel koneksi php

--- includes/koneksi.php
+++ includes/koneksi.php
@@ -7,4 +7,4 @@ $user = "root";
 $pass = "";
 $database = "db_perpus";
 
-$conn = mysqli_connect($host, $user, $pass, $database) or die("gagal koneksi ke database");
+$db = mysqli_connect($host, $user, $pass, $database) or die("gagal koneksi ke database");
